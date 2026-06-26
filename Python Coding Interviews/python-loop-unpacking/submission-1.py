a1610522e33b6cc75fb5bc67a8d2a39c1730c6bb
Henry Hieu Nguyen <119148881+hieunguyen601@users.noreply.github.com>
2026-06-26 16:22:28 -0500

    Add: python-loop-unpacking - submission-1

---
+++ Python Coding Interviews/python-loop-unpacking/submission-1.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    student, highest_score = "", 0
+    for name, score in scores:
+        if highest_score < score:
+            student, highest_score = name, score
+    return student
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
